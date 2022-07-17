898d849f9f72833b05a2071e015b5945bea6a986
adrianohpmaciel <79804366+adrianohpmaciel@users.noreply.github.com>
2022-07-17 14:34:18 -0300

    ADD -> Projeto inicial com base em uma arquitetura cebola, possuindo as entidades já implementadas

---
+++ SAGOM.Domain/Entities/Bill.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAGOM.Domain.Entities
+{
+    public sealed class Bill
+    {
+        public int Id { get; set; }
+        public Company ReceiverCompany { get; private set; }
+        public Company PayerCompany { get; private set; }
+        public Person Payer { get; private set; } 
+        public string Description { get; private set; }
+        public double Value { get; private set; }
+        public DateTime Date { get; private set; }
+        public DateTime DueDate { get; private set; }
+        public DateTime PaymentDate { get; private set; }
+        public string Status { get; private set; }
+
+    }
+}
---
+++ SAGOM.Domain/Entities/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAGOM.Domain.Entities
+{
+    public sealed class Client
+    {
+        public int Id { get; private set; }
+        public Person Person { get; private set; }
+    }
+}
